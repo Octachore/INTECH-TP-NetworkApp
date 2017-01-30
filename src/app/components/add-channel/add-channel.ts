2faf7092dcfb4027f55daf9508de0767db5d6d33
Octachore <octachore@outlook.com>
2017-01-30 14:32:22 +0100

    channel

--- src/app/components/add-channel/add-channel.ts
+++ src/app/components/add-channel/add-channel.ts
@@ -9,20 +9,36 @@ import { ChannelService } from 'services';
     templateUrl: 'add-channel.html'
 })
 export class AddChannelComponent {
-    @ViewChild(ModalDirective) 
-    modal:ModalDirective;
-    @ViewChild(NgForm) 
-    ngForm:NgForm;
+    @ViewChild(ModalDirective)
+    modal: ModalDirective;
+    @ViewChild(NgForm)
+    ngForm: NgForm;
 
-    model =  {name: ''};
-    
-    constructor(
-    ) {
+    model = { name: '' };
+
+    message: string;
+
+    constructor(private channelService: ChannelService) {
+    }
+
+    close() {
+        this.message = '';
+        this.modal.hide();
     }
 
     save() {
+        const name = this.model.name;
         if (this.ngForm.valid) {
-      
+            this.channelService.getAll().then((channels) => {
+                if (channels.map(c => c.name).indexOf(name) > -1) {
+                    this.message = 'Name already exists';
+                    return;
+                }
+                else {
+                    this.channelService.add(this.model.name);
+                    this.modal.hide();
+                }
+            });
         }
     }
 }
